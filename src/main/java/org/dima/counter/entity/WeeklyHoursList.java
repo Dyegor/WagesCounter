1667058b77ec962787b13a5098371c1ceb7c9259
Dyegor <yegorov.dmt@gmail.com>
2020-05-10 00:09:00 +1200

    Variable 'totalHours' moved from weeklyPayment to weeklyHoursList class

--- src/main/java/org/dima/counter/entity/WeeklyHoursList.java
+++ src/main/java/org/dima/counter/entity/WeeklyHoursList.java
@@ -1,11 +1,14 @@
 package org.dima.counter.entity;
 
+import org.dima.counter.entity.payments.WeeklyPayment;
+
 import java.util.List;
 
 public class WeeklyHoursList {
 
     private List<DailyReport> dailyReportsList;
     private  String weekEndingDate;
+    private  double totalHours;
 
     public List<DailyReport> getDailyReportsList() {
         return dailyReportsList;
@@ -21,5 +24,13 @@ public class WeeklyHoursList {
 
     public void setWeekEndingDate(String weekEndingDate) {
         this.weekEndingDate = weekEndingDate;
+    }
+
+    public double getTotalHours() {
+        return totalHours;
+    }
+
+    public void setTotalHours(double totalHours) {
+        this.totalHours = totalHours;
     }
 }
--- src/main/java/org/dima/counter/entity/payments/WeeklyPayment.java
+++ src/main/java/org/dima/counter/entity/payments/WeeklyPayment.java
@@ -2,6 +2,7 @@ package org.dima.counter.entity.payments;
 
 import org.dima.counter.buisnessLogic.WagesCalculator;
 import org.dima.counter.entity.DailyReport;
+import org.dima.counter.entity.WeeklyHoursList;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -15,9 +16,17 @@ public class WeeklyPayment extends Payment {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
 
-    public double calculateTotalHours(DailyReport dailyReport, WeeklyPayment weeklyPayment) {
+    public WeeklyPayment populateWeek(WeeklyPayment weeklyPayment) {
+        weeklyPayment.setGrossEarnings(WagesCalculator.calculateGrossEarnings(weeklyPayment.getTotalHours()));
+        weeklyPayment.setPaye(WagesCalculator.calculatePaye(weeklyPayment.getGrossEarnings()));
+        weeklyPayment.setAccAmount(WagesCalculator.calculateAcc(weeklyPayment.getGrossEarnings()));
+        weeklyPayment.setNetPay(weeklyPayment.getGrossEarnings() - weeklyPayment.getPaye());
+        return weeklyPayment;
+    }
+
+    public double calculateTotalHours(DailyReport dailyReport, WeeklyHoursList weeklyHoursList) {
         double dailyHours = dailyReport.getHoursDone();
-        double totalHours = weeklyPayment.getTotalHours();
+        double totalHours = weeklyHoursList.getTotalHours();
 
         if (dailyReport.getDay().equals("Saturday") || dailyReport.getDay().equals("Sunday")
                 || totalHours > 45) {
@@ -28,13 +37,5 @@ public class WeeklyPayment extends Payment {
             totalHours += dailyHours;
         }
         return totalHours;
-    }
-
-    public WeeklyPayment populateWeek(WeeklyPayment weeklyPayment) {
-        weeklyPayment.setGrossEarnings(WagesCalculator.calculateGrossEarnings(weeklyPayment.getTotalHours()));
-        weeklyPayment.setPaye(WagesCalculator.calculatePaye(weeklyPayment.getGrossEarnings()));
-        weeklyPayment.setAccAmount(WagesCalculator.calculateAcc(weeklyPayment.getGrossEarnings()));
-        weeklyPayment.setNetPay(weeklyPayment.getGrossEarnings() - weeklyPayment.getPaye());
-        return weeklyPayment;
     }
 }
