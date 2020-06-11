cadcd9b8ca3c904a7e34b077b894ceeaa648c05b
Dyegor <yegorov.dmt@gmail.com>
2020-06-11 20:42:20 +1200

    JUnit test created for HoursCounterTest class

--- src/test/java/org/dima/counter/buisnessLogic/HoursCounterTest.java
+++ src/test/java/org/dima/counter/buisnessLogic/HoursCounterTest.java
@@ -1,5 +1,16 @@
+package org.dima.counter.buisnessLogic;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HoursCounterTest {
 
+    @Test
+    void calculateAmountOfHours() {
+    }
+
+    @Test
+    void calculateTotalHours() {
+    }
 }
