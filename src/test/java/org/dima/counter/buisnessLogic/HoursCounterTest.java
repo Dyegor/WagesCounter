f9644e19ab796ceead70bc2a2a329f4f1739a1e9
Dyegor <yegorov.dmt@gmail.com>
2020-06-24 20:02:51 +1200

    Unit tests implemented for HoursCounter class methods

--- src/test/java/org/dima/counter/buisnessLogic/HoursCounterTest.java
+++ src/test/java/org/dima/counter/buisnessLogic/HoursCounterTest.java
@@ -1,16 +1,36 @@
 package org.dima.counter.buisnessLogic;
 
+import org.dima.counter.entity.DailyReport;
+import org.dima.counter.entity.WeeklyTimeSheet;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HoursCounterTest {
 
+    private static DailyReport dailyReport;
+    private static WeeklyTimeSheet weeklyTimeSheet;
+
+    @BeforeAll
+    static void setUp() {
+        dailyReport = new DailyReport();
+        dailyReport.setStartTime("10:30");
+        dailyReport.setFinishTime("22:30");
+        dailyReport.setHoursDone(5);
+        dailyReport.setDay("Saturday");
+
+        weeklyTimeSheet = new WeeklyTimeSheet();
+        weeklyTimeSheet.setTotalHours(40);
+    }
+
     @Test
     void calculateAmountOfHours() {
+        assertEquals(12, HoursCounter.calculateAmountOfHours(dailyReport), "Returns calculated amount of hours");
     }
 
     @Test
     void calculateTotalHours() {
+        assertEquals(47.5, HoursCounter.calculateTotalHours(dailyReport, weeklyTimeSheet), "Returns calculated total hours");
     }
 }
