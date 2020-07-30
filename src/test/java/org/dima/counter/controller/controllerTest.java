b471e7d5e77cf56786b41fb537e61d33ffb2165b
Dyegor <yegorov.dmt@gmail.com>
2020-07-30 19:40:53 +1200

    Created JUnit test for Controller class

--- src/test/java/org/dima/counter/controller/controllerTest.java
+++ src/test/java/org/dima/counter/controller/controllerTest.java
@@ -1,26 +1,66 @@
-package controller;
+package org.dima.counter.controller;
 
-import org.apache.tomcat.util.http.parser.MediaType;
-import org.aspectj.lang.annotation.Before;
-import org.dima.counter.controller.CounterController;
+import org.dima.counter.entity.PaySlip;
+import org.dima.counter.service.CounterService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
+@ExtendWith(SpringExtension.class)
+@WebMvcTest(CounterController.class)
 public class controllerTest {
 
-    private MockMvc mockMvc;
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    @MockBean
+    private CounterService counterService;
 
     @BeforeEach
     public void setup() {
-        this.mockMvc = standaloneSetup(new CounterController()).build();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac).build();
     }
 
     @Test
-    public void testSayHelloWorld() throws Exception {
-        this.mockMvc.perform(get("/success").andExpect(status().isOk()));
+    public void testAddWeekPage() throws Exception {
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/counter/addWeek"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testPaySlipsListPage() throws Exception {
+        List<String> paySlipsList = Arrays.asList("10-11-2020", "11-11-2020", "12-11-2020");
+
+        given(counterService.getPaySlipsList()).willReturn(paySlipsList);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/counter/paySlipsList"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testYearlyReportsPage() throws Exception {
+        PaySlip yearlyReport = new PaySlip();   
+
+        given(counterService.getYearlyPayments()).willReturn(yearlyReport);
+
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/counter/yearlyReport"))
+                .andExpect(status().isOk());
     }
 }
